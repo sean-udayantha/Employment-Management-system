e28838cc0ed5d7ccc7e5856610776e07ab14d3e8
sean-udayantha <seanudayantha@mail.com>
2023-03-07 11:18:31 +0530

    add backend

---
+++ backend/server.js
@@ -0,0 +1,68 @@
+import {} from "dotenv/config";
+import express, { json } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+
+const { connect } = mongoose;
+
+// Connect MongoDB.
+mongoose.set("strictQuery", false);
+const URI = process.env.MONGODB_URL;
+connect(
+  URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB Connection Succeeded.");
+    } else {
+      console.log("Error in DB connection: " + err);
+    }
+  }
+);
+
+const app = express();
+app.use(json());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.set("trust proxy", 1);
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  `Server running on port ${port} 🔥`;
+  console.log(`Server running on port ${port} 🔥`);
+});
+
+//route imports
+
+
+
+//Doctor route
+import userRoute from "./routes/user.js";
+//patient route
+import patientRoute from "./routes/patient.js";
+
+//routes
+
+
+//Doctor routes
+app.use(userRoute);
+//patient routes
+app.use(patientRoute);
+
+
+  
